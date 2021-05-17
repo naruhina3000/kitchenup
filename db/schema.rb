dba783b2647bcba1882387ae6289c398e0882a10
Shari Breitenstein-Werner <shari_breitenstein@yahoo.de>
2021-05-17 16:38:37 +0800

    Kitchen model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_073923) do
+ActiveRecord::Schema.define(version: 2021_05_17_083557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "kitchens", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "size"
+    t.string "listing_status"
+    t.text "address"
+    t.integer "price"
+    t.string "cancellation_policy"
+    t.time "opening_time"
+    t.time "closing_time"
+    t.integer "views"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_kitchens_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
@@ -36,4 +53,5 @@ ActiveRecord::Schema.define(version: 2021_05_17_073923) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "kitchens", "users"
 end
