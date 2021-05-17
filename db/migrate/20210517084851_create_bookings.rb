29fd03c850a35e09cd9e78fca303a4722d36f78e
Shari Breitenstein-Werner <shari_breitenstein@yahoo.de>
2021-05-17 16:54:52 +0800

    Created models: bookings, reviews, favorites, amenities, kitchen_amenities

---
+++ db/migrate/20210517084851_create_bookings.rb
@@ -0,0 +1,15 @@
+class CreateBookings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bookings do |t|
+      t.date :start_date
+      t.date :end_date
+      t.integer :total_price
+      t.string :confirmation_code
+      t.string :booking_status, default: "pending"
+      t.references :user, null: false, foreign_key: true
+      t.references :kitchen, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_083557) do
+ActiveRecord::Schema.define(version: 2021_05_17_085234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "amenities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "total_price"
+    t.string "confirmation_code"
+    t.string "booking_status", default: "pending"
+    t.bigint "user_id", null: false
+    t.bigint "kitchen_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["kitchen_id"], name: "index_bookings_on_kitchen_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "kitchen_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["kitchen_id"], name: "index_favorites_on_kitchen_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "kitchen_amenities", force: :cascade do |t|
+    t.bigint "kitchen_id", null: false
+    t.bigint "amenity_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["amenity_id"], name: "index_kitchen_amenities_on_amenity_id"
+    t.index ["kitchen_id"], name: "index_kitchen_amenities_on_kitchen_id"
+  end
 
   create_table "kitchens", force: :cascade do |t|
     t.string "title"
@@ -30,6 +68,18 @@ ActiveRecord::Schema.define(version: 2021_05_17_083557) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_kitchens_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.text "reply"
+    t.bigint "user_id", null: false
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,5 +103,13 @@ ActiveRecord::Schema.define(version: 2021_05_17_083557) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "kitchens"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "favorites", "kitchens"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "kitchen_amenities", "amenities"
+  add_foreign_key "kitchen_amenities", "kitchens"
   add_foreign_key "kitchens", "users"
+  add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "users"
 end
