8d8e24d3daa36abf7b4b01d97f3d0335213a30d5
Jacqueline <j.sutjiawan@gmail.com>
2021-05-17 17:46:20 +0800

    vlaidates reviews & reply on review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ApplicationRecord
   belongs_to :booking
 
   validates :rating, presence: true
-  validates :content, presence: true
-  validates :reply, presence: true
-
+  validates :content, length: { minimum: 20 }
+  validates :reply, length: { minimum: 20 }, on: :update
 end
+
