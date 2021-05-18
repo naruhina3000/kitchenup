af46b5a2e7e506d3a3b334e8d3a8965cb67011c6
Jacqueline <j.sutjiawan@gmail.com>
2021-05-18 15:19:48 +0800

    skip before action user authentication for index and show kitchen

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -1,4 +1,5 @@
 class KitchensController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_kitchen, only: [:show, :edit, :update, :destroy]
 
   def index
