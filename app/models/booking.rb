f00f90e283e6d841eeca080d22171987c2a89890
Shari Breitenstein-Werner <shari_breitenstein@yahoo.de>
2021-05-18 17:30:46 +0800

    seed for all models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,16 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :kitchen
-  has_one :review
+  has_one :review, dependent: :destroy
 
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :confirmation_code, uniqueness: true
+
+  before_validation :generate_confirmation_code
+
+  def generate_confirmation_code
+    self.confirmation_code = rand(36**6).to_s(36).upcase
+  end
+
 
 end
