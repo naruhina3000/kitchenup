786d277c60c14d5e47a29c5960b81ddf90d60225
Shari Breitenstein-Werner <shari_breitenstein@yahoo.de>
2021-05-18 11:33:01 +0800

    kitchen controller done, bookings controller started

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -12,21 +12,40 @@ class KitchensController < ApplicationController
   end
 
   def update
+    if @kitchen.update(kitchen_params)
+      redirect_to @kitchen
+    else
+    render "edit"
   end
 
   def new
+    @kitchen = Kitchen.new
   end
 
   def create
+    @kitchen = Kitchen.new(kitchen_params)
+    @user = current_user
+    @kitchen.user = @user
+    if @kitchen.save
+    redirect_to @kitchen
+    else 
+     render 'new'
+    end
   end
 
   def destroy
+    @ktchen.destroy
+    redirect_to kitchens_path
   end
 
   private
 
   def set_kitchen
-    params.require(:list).permit(:title, :description, :size, :listing_status,
+    @kitchen = Kitchen.find(params[:id])
+  end
+
+  def kitchen_params
+    params.require(:booking).permit(:title, :description, :size, :listing_status,
     :address, :price, :cancellation_policy, :opening_time, :closing_time, :photos)
   end
 end
