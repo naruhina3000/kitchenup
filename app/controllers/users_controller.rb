a6591a7c9837830fecf7ffb69e910a6edb0b929c
Shari Breitenstein-Werner <shari_breitenstein@yahoo.de>
2021-05-20 18:53:17 +0800

    edit user and profile picture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,14 +12,22 @@ class UsersController < ApplicationController
     end
 
     def edit
+      @user = User.find(params[:id])
     end
-
+  
     def update
+      @user = User.find(params[:id])
+  
+      if @user.update(user_params)
+        redirect_to @user
+      else
+        render "edit"
+      end
     end
 
     private
 
     def user_params
-      params.require(:user).permit(:photo)
+      params.require(:user).permit(:first_name, :last_name, :phone_number, :photo)
     end
 end
